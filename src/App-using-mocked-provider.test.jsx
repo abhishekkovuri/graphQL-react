c737eccfd00e1fc24d00f4eb4e8f936ddff144e0
Abhishek Kovuri <abhishekkovuri25@gmail.com>
2022-12-08 14:37:29 +0530

    added few test cases

--- src/App-using-mocked-provider.test.jsx
+++ src/App-using-mocked-provider.test.jsx
@@ -1,9 +1,15 @@
-import { render, waitFor } from "@testing-library/react"
-import { MockedProvider } from "@apollo/client/testing"
-import App from "./App"
-import { GET_COUNTRIES } from "./graphql-query/getCountries"
+import {
+	render,
+	waitFor,
+	fireEvent,
+	getAllByText,
+} from '@testing-library/react';
+import { MockedProvider } from '@apollo/client/testing';
+import App from './App';
+import { GET_COUNTRIES } from './graphql-query/getCountries';
+import { GET_COUNTRIES_V2 } from './graphql-query/getCountriesV2';
 
-describe("App test", () => {
+describe('App test', () => {
 	const mocks = [
 		{
 			request: {
@@ -14,27 +20,71 @@ describe("App test", () => {
 					getCountries: {
 						countries: [
 							{
-								countryName: "India",
-								capital: "New Delhi",
+								countryName: 'India',
+								capital: 'New Delhi',
 							},
 						],
 					},
 				},
 			},
 		},
-	]
-	test("renders learn react link", async () => {
+		{
+			request: {
+				query: GET_COUNTRIES_V2,
+			},
+			result: {
+				data: {
+					getCountriesV2: {
+						countries: [
+							{
+								countryName: 'Japan',
+								capital: 'Tokyo',
+							},
+						],
+					},
+				},
+			},
+		},
+	];
+
+	test('should generate snapshot', async () => {
+		const { asFragment } = render(
+			<MockedProvider mocks={[...mocks]}>
+				<App />
+			</MockedProvider>
+		);
+
+		await waitFor(() => expect(asFragment()).toMatchSnapshot());
+	});
+
+	test('should have the countries list generated', async () => {
 		const { getByText, queryByText } = render(
 			<MockedProvider mocks={[...mocks]}>
 				<App />
 			</MockedProvider>
-		)
-		expect(getByText(/Render Country List/i)).toBeInTheDocument()
+		);
+
+		expect(getByText(/Render Country List/i)).toBeInTheDocument();
 		await waitFor(() =>
 			expect(getByText(/India - New Delhi/i)).toBeInTheDocument()
-		)
+		);
 		await waitFor(() =>
 			expect(queryByText(/Japan - Tokyo/i)).not.toBeInTheDocument()
-		)
-	})
-})
+		);
+	});
+
+	test('should display duplicate countries list upon click', async () => {
+		const { getAllByText, getByText } = render(
+			<MockedProvider mocks={[...mocks]}>
+				<App />
+			</MockedProvider>
+		);
+
+		const buttonElement = getByText(/Click me/);
+		fireEvent.click(buttonElement);
+
+		await waitFor(() =>
+			expect(getAllByText(/India - New Delhi/i).length).toBe(2)
+		);
+	});
+});
--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React from "react"
-import { useQuery } from "@apollo/client"
-import { GET_COUNTRIES } from "./graphql-query/getCountries.js"
-import "./App.css"
+import React from 'react';
+import { useLazyQuery, useQuery } from '@apollo/client';
+import { GET_COUNTRIES } from './graphql-query/getCountries.js';
+import { GET_COUNTRIES_V2 } from "./graphql-query/getCountriesV2"
+import './App.css';
 
 const App = () => {
-	const { data } = useQuery(GET_COUNTRIES)
-	const countries = data?.getCountries?.countries
+	const { data } = useQuery(GET_COUNTRIES);
+	const countries = data?.getCountries?.countries;
+
+	const { data: countriesData } = useQuery(GET_COUNTRIES_V2);
+	const anotherCountryList = countriesData?.getCountries?.countries;
+
+	const [lazyLoadBtn, { data: duplicateCountriesData }] =
+		useLazyQuery(GET_COUNTRIES);
+	const duplicateCountries = duplicateCountriesData?.getCountries?.countries;
 
 	return (
 		<section className="list">
 			<header> Render Country List </header>
 			<section>
+				<h1>Original data</h1>
 				{countries &&
 					countries.map((country) => (
 						<div className="item" key={country.countryName}>
@@ -18,8 +27,27 @@ const App = () => {
 						</div>
 					))}
 			</section>
+			<button onClick={lazyLoadBtn}>Click me</button>
+			<section>
+				<h1>Duplicate data</h1>
+				{duplicateCountries &&
+					duplicateCountries.map((country) => (
+						<div className="item" key={country.countryName}>
+							{country.countryName} - {country.capital}
+						</div>
+					))}
+			</section>
+			<section>
+				<h1>Another set of countries</h1>
+				{anotherCountryList &&
+					anotherCountryList.map((country) => (
+						<div className="item" key={country.countryName}>
+							{country.countryName} - {country.capital}
+						</div>
+					))}
+			</section>
 		</section>
-	)
-}
+	);
+};
 
-export default App
+export default App;
